f336122517788dcb30e61ffb43911e320396bec9
Misha <darkrabbit240@gmail.com>
2026-02-24 16:25:14 +0200

    feat: add balance container

--- frontend/src/app/(drawer)/index.tsx
+++ frontend/src/app/(drawer)/index.tsx
@@ -3,10 +3,16 @@ import { Text, View, StyleSheet } from "react-native";
 import Button from "@/components/Button";
 
 export default function Index() {
+  const money = 10;
+
   return (
     <View style={styles.screen}>
       <View style={styles.buttonGroupContainer}>
         <Button label={'minus'} onPress={() => console.log('Button 1')} />
+        <View style={styles.balanceGroupContainer} >
+          <Text style={styles.balanceTitle}>Balance:</Text>
+          <Text style={styles.balance}>{money}</Text>
+        </View>
         <Button label={'plus'} onPress={() => console.log('Button 2')} />
       </View>
     </View>
@@ -25,10 +31,26 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    width: 300,
+    width: 360,
     height: 150,
     padding: 20,
     borderRadius: 12,
+    gap: 10,
   },
+  balanceGroupContainer: {
+    backgroundColor: '#3a3257',
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    padding: 3,
+  },
+  balanceTitle: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  balance: {
+    color: '#fff',
+    fontSize: 40,
+  }
 })
 
