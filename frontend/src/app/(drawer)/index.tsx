d93ad97f8dcee031ab9462b6b6f938eb16abbba6
Misha <darkrabbit240@gmail.com>
2026-02-24 16:12:46 +0200

    feat: add button component

--- frontend/src/app/(drawer)/index.tsx
+++ frontend/src/app/(drawer)/index.tsx
@@ -1,21 +1,34 @@
 import { Text, View, StyleSheet } from "react-native";
 
+import Button from "@/components/Button";
+
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text} >Home screen</Text>
+    <View style={styles.screen}>
+      <View style={styles.buttonGroupContainer}>
+        <Button label={'minus'} onPress={() => console.log('Button 1')} />
+        <Button label={'plus'} onPress={() => console.log('Button 2')} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: '#2f2e33',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  text: {
-    color: '#fff',
-  }
+  buttonGroupContainer: {
+    backgroundColor: '#262236',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 300,
+    height: 150,
+    padding: 20,
+    borderRadius: 12,
+  },
 })
+
---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,93 @@
+import { Pressable, StyleSheet, Text, type ViewStyle } from 'react-native';
+
+type ButtonSize = 'small' | 'medium' | 'large';
+
+type Props = {
+  label: string;
+  size?: ButtonSize;
+  onPress?: () => void;
+  style?: ViewStyle;
+  disabled?: boolean;
+}
+
+export default function Button({
+  label,
+  size = 'medium',
+  onPress,
+  style,
+  disabled = false
+}: Props) {
+
+  const sizeStyles = {
+    small: styles.buttonSmall,
+    medium: styles.buttonMedium,
+    large: styles.buttonLarge,
+  };
+
+  const textSizeStyles = {
+    small: styles.textSmall,
+    medium: styles.textMedium,
+    large: styles.textLarge,
+  };
+
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        styles.buttonBase,
+        sizeStyles[size],
+        style,
+      ]}
+      onPress={onPress}
+      disabled={disabled}
+    >
+      <Text
+        style={[
+          styles.textBase,
+          textSizeStyles[size],
+        ]}
+      >
+        {label}
+      </Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  // Base styles
+  buttonBase: {
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  textBase: {
+    fontWeight: '600',
+    color: '#000',
+    textAlign: 'center',
+  },
+
+  // Size variants (fixed dimensions for perfect circles)
+  buttonSmall: {
+    width: 60,
+    height: 60,
+  },
+  buttonMedium: {
+    width: 80,
+    height: 80,
+  },
+  buttonLarge: {
+    width: 100,
+    height: 100,
+  },
+
+  textSmall: {
+    fontSize: 14,
+  },
+  textMedium: {
+    fontSize: 16,
+  },
+  textLarge: {
+    fontSize: 18,
+  },
+
+})
