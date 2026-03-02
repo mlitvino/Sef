fc16f91749eb6217068cab92fe56878da1444782
Misha <darkrabbit240@gmail.com>
2026-03-02 18:51:38 +0200

    feat: add useEffect to change background color

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
 import { setBackgroundColorAsync } from 'expo-system-ui';
 
-setBackgroundColorAsync('#222025')
-  .catch(err => console.error('Failed to change root background color:', err));
-
 export default function RootLayout() {
+  useEffect(() => {
+    setBackgroundColorAsync('#222025')
+      .catch(err => console.error('Failed to change root background color:', err));
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name={'(drawer)'} options={{ headerShown: false }}  />
