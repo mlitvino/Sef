a86fd8b108dc6fdbd63bbe62811f478ee1f74b26
Misha <darkrabbit240@gmail.com>
2026-03-02 18:23:28 +0200

    fix: set background color to prevent white flickers

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,4 +1,8 @@
 import { Stack } from 'expo-router';
+import { setBackgroundColorAsync } from 'expo-system-ui';
+
+setBackgroundColorAsync('#222025')
+  .catch(err => console.error('Failed to change root background color:', err));
 
 export default function RootLayout() {
   return (
