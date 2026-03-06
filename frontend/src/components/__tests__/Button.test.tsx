99d36347d4c8a782e6bccf9602d716b4b2279a09
Misha <darkrabbit240@gmail.com>
2026-03-07 00:30:56 +0200

    test: add ThemeProvider for Button

--- frontend/src/components/__tests__/Button.test.tsx
+++ frontend/src/components/__tests__/Button.test.tsx
@@ -1,12 +1,17 @@
 import { render } from '@testing-library/react-native';
 
+import { AppProviders } from '@/contexts/AppProviders';
 import Button from '@/components/Button';
 
 describe('Button component', () => {
   test('Button renders correctly', () => {
     const text = 'vampire';
 
-    const tree = render(<Button label={text} />);
+    const tree = render(
+      <AppProviders>
+        <Button label={text} />
+      </AppProviders>,
+    );
 
     expect(tree.getByText(text)).toBeVisible();
     expect(tree).toMatchSnapshot();
